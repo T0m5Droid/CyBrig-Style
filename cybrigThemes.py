4cb39ffebac6ce4a1fc0866554407a93eee77328
CyBrig <84447155+T0m5Droid@users.noreply.github.com>
2021-07-24 10:47:36 +0800

    Create cybrigThemes.py

---
+++ cybrigThemes.py
@@ -0,0 +1,67 @@
+# Code Warna •
+#   \033[90m     -    Abu-Abu
+#   \033[91m     -    Merah
+#   \033[92m     -    Hijau
+#   \033[93m     -    Kuning
+#   \033[94m     -    Biru Tua
+#   \033[95m     -    Ungu
+#   \033[96m     -    Biru
+#   \033[97m     -    Putih
+ 
+import os,sys,time
+def teks(wkwk):
+    for haha in wkwk + '\n':
+        sys.stdout.write(haha)
+        sys.stdout.flush()
+        time.sleep(0.0001)
+ 
+os.system("clear")
+teks("""\033[91m
+                                                                                            
+                                                                                
+                                                                             
+                                                                             
+                                      :+o/.                                 
+                                  .:oyy+:-+shs/-`                               
+                            .:+shs+-`       -/shyo/-                  
+                     .:/+syys+:.                .:/oyhso/:-.                  
+            /+oosyyyso+/-.`                           .-:/+osyyyso+/           
+            yh-``                 `:sdy/.                       `.yd.          
+            yh                 .+hNh+-+hNms/`                     yd`          
+            sh`             :smms:`      -ohNdo:                  hh`          
+            od.         `+hNh+.             `:mM.                 hh           
+            +h-                        `      mM`                `hy           
+            /h:  `/yNdo.           `/yNs      NM      .-:/+:     .hs           
+            :h/  sMd`           -odMdo-       NN     ./yhh:      -ho           
+            .h+   +My       `/ymNy/`         `Mm  -+so:`/`       /h+           
+            `hs    :Nh`     +Mh-            -oNdos/.             +h:           
+             sh     -Nm.    +Mo         `:sdmdmmmy/`             yh`           
+             :h:     .mN-   +Ms      .+hmmdmds:`-+hNds:`        .ho            
+              ys      `hN:  /Ms     /mddmh+.        :odNdo.     oh-\033[97m            
+              /h-       ym  /Ms `:os+:o/`              `hM:    .hs             
+               yy        :  /Nhso:`                     yM-    oh.             
+               .h+         oysmms:           `/yNd-     dM.   :h+              
+                /h-            :smNh+.    -odMdo-       mM`  .hs               
+                 oh.        `s+.  .+hNdoyNNy/`          mM  `yy`                
+                  sy.        `dN:    `/yo-           `:sMd `sy.                
+                   oh-        `yM/                .+hNh+. .yy`                 
+                    /h:         sMo           `:smms:`   -hs`                  
+                     -h+`        +My         /Nh+.      /h/                    
+                      `oy-        :Nh` `:o` `:`       .sy.                     
+                        :yo`       -NmhNh-          `+h+                        
+                         `/h+`      .s:`          `/ho.                        
+                           `+y+.                `+ho.                          
+                             `:ys-            -oh+`                            
+                                -oy+.      ./ys:                              
+                                 `:syo::+ys/.                                
+                                    `-++:`
+ 
+\033[91m ██████╗██╗   ██╗██████╗ ███████╗██████╗     \033[97m██████╗ ██████╗ ██╗ ██████╗  █████╗ ██████╗ ███████╗
+\033[91m██╔════╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔══██╗    \033[97m██╔══██╗██╔══██╗██║██╔════╝ ██╔══██╗██╔══██╗██╔════╝
+\033[91m██║      ╚████╔╝ ██████╔╝█████╗  ██████╔╝    \033[97m██████╔╝██████╔╝██║██║  ███╗███████║██║  ██║█████╗  
+\033[91m██║       ╚██╔╝  ██╔══██╗██╔══╝  ██╔══██╗    \033[97m██╔══██╗██╔══██╗██║██║   ██║██╔══██║██║  ██║██╔══╝  
+\033[91m╚██████╗   ██║   ██████╔╝███████╗██║  ██║    \033[97m██████╔╝██║  ██║██║╚██████╔╝██║  ██║██████╔╝███████╗
+\033[91m ╚═════╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝    \033[97m╚═════╝ ╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝                                   
+""")
+ 
+print(teks)
